2947863d0610ce85663ed13ef4a0bd3d0ac5a284
rameenhamid <rameen.hamid@dubizzlelabs.com>
2023-11-05 14:13:55 +0500

    basic file structure

--- util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('linkedin-complete', 'root', '', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
